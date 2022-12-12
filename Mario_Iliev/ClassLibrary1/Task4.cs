756769543783a0655875214fe077c9b0032d1618
Mario Iliev <Mario_Iliev@epam.com>
2022-12-12 12:28:26 +0200

    C# home task - Task 4 Solution

---
+++ Mario_Iliev/ClassLibrary1/Task4.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Task4
+    {
+        [Test]
+        public void TestCount()
+        {
+            int[] array = new int[] { 1, 3, 6, 2, 2, 0, 4, 5 };
+            int target = 5;
+            int ActualResult = CountPairs(array, target);
+            int ExpetedResult = 4;
+            Assert.AreEqual(ExpetedResult, ActualResult);
+
+        }
+        public static int CountPairs(int[] arr, int target)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                    if ((arr[i] + arr[j]) == target)
+                        count++;
+            return count;
+        }
+    }
+}
