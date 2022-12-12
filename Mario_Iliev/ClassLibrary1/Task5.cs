ca6bad89aec1efc60e16547f125cee6765c10b29
Mario Iliev <Mario_Iliev@epam.com>
2022-12-12 12:37:09 +0200

    C# home task - Task 5 Solution

---
+++ Mario_Iliev/ClassLibrary1/Task5.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Task5
+    {
+        [Test]
+        public void FriensListTest()
+        {
+            string s = "Fred:Corwill;Wilfred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+            string FriendsListAfterProcessing = ProccesingFriendsList(s);
+            string ExpectedList = "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)";
+            Assert.AreEqual(ExpectedList, FriendsListAfterProcessing);
+        }
+
+        public string ProccesingFriendsList(string friends)
+        {
+            return "(" + string.Join(")(",
+                friends.
+                ToUpper().
+                Split(';').
+                Select(n => n.Split(":")).
+                Select(n => string.Join(", ", n.Reverse())).
+                OrderBy(n => n)
+            ) + ")";
+        }
+    }
+}
