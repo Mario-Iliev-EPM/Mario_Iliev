cea4724db60008e78fc787844a9706e996ccf57c
Mario Iliev <Mario_Iliev@epam.com>
2023-01-03 10:53:27 +0200

    Fix task 3 - digital root

--- Mario_Iliev/ClassLibrary1/Task3.cs
+++ Mario_Iliev/ClassLibrary1/Task3.cs
@@ -9,34 +9,32 @@ namespace ClassLibrary1
 {
     public class Task3
     {
-        [TestCase(16, 7)]
-
+        [TestCase(1234567, 1)]
         public void TestDigRootTest1(int input1, int expectedResult)
         {
             int number = DigitalRootNumber(input1);
 
             Assert.AreEqual(expectedResult, number);
         }
-        [TestCase(132189, 6)]
 
+        [TestCase(132189, 6)]
         public void TestDigRootTest2(int input1, int expectedResult)
         {
             int number = DigitalRootNumber(input1);
 
             Assert.AreEqual(expectedResult, number);
         }
+
         public static int DigitalRootNumber(int n)
         {
             int root = 0;
-            while (n % 10 > 0)
+            while (n > 0)
             {
                 root += (n % 10);
                 n /= 10;
-
             }
             if (root / 10 > 0)
             {
-
                 return DigitalRootNumber(root);
             }
             return root;
